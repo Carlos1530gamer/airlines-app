ec7394bad291be8aeca666b0e5fc188eadc8e995
Carlos Daniel Hernandez Chauteco <gamercarlos1530@gmail.com>
2024-09-20 00:06:39 -0600

    Initial Commit

---
+++ app-airlines/app_airlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_airlinesApp.swift
+//  app-airlines
+//
+//  Created by Carlos Daniel Hernandez Chauteco on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct app_airlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
